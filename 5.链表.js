fb189cb9375e9ea4057d2becf1995bb90241fac3
zoexyf <1270355646@qq.com>
2019-07-14 23:23:55 +0800

    insert

--- 5.链表.js
+++ 5.链表.js
@@ -23,10 +23,40 @@ function LinkedList(){
         
         length=length+1
     }
-    this.insert= function(position,element){}
+    //insert(position,element):向列表的特定位置插入一个新的项
+    this.insert= function(position,element){
+        if(position>=0&&position<=length){
+            var node=new Node(element),
+            current=head,
+            previous,
+            index=0;
+
+            if(position===0){
+                node.next=current
+                head=node
+            }else{
+                while(index++<position){
+                    previous=current
+                    current=current.next
+                }
+                node.next=current
+                previous.next=node
+            }
+            length++
+            return true
+        }else{
+            return false
+        }
+
+    }
     this.removeAt=function(position){}
     this.remove=function(element){}
     this.indexOf=function(element){}
 
 
 }
+
+// var list=new LinkedList()
+// list.append(15)
+// list.append(10)
+// console.log(list)
